50b29d8206418b1d6e5f5510b09f5d574d2c7a4d
Mimadfaoussi <imadfaoussi@gmail.com>
2023-11-10 07:30:15 -0800

    creating the header file and include necessary libraries

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#endif
