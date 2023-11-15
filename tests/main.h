1b40ba00642016610d928d9c2aa6231b2a644adc
Imad Faoussi <93826012+Mimadfaoussi@users.noreply.github.com>
2023-11-15 11:30:19 +0100

    Delete tests directory

--- tests/main.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-void find_file_path(char *filename);
-char    **ft_split(char const *str, char c);
-size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t  ft_strlen(const char *s);
